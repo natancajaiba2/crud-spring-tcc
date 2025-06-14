7f72ed6343e566ce23214ecb32aea805bd1e014d
natancajaiba2 <natan.cajaiba@etec.sp.gov.br>
2025-06-14 15:25:10 -0300

    api crud feito em springboot

---
+++ src/main/java/br/com/projeto/api/modelo/Cliente.java
@@ -0,0 +1,29 @@
+package br.com.projeto.api.modelo;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nome;
+
+    private String email;
+
+    private int idade;
+
+    private String plano;
+}
